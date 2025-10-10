27f5ca8e712a9c9e32aa472e6480c0d7a0e2e351
mosson <cucation@gmail.com>
2025-10-10 18:41:12 +0900

    add simple_object example

---
+++ src/bin/simple_object.rs
@@ -0,0 +1,51 @@
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject};
+
+#[derive(SimpleObject)]
+#[allow(dead_code)]
+struct MyObject {
+    /// Value a docコメントはSDLに反映される
+    a: i32,
+
+    b: i32,
+
+    #[graphql(skip)]
+    c: i32,
+}
+
+struct Query;
+
+#[Object]
+impl Query {
+    // デフォルトでキャメルケースになる
+    // カスタムの名前はマクロで登録可能
+    #[graphql(name = "hoge")]
+    async fn my_object(&self) -> MyObject {
+        MyObject {
+            a: 42,
+            b: 24,
+            c: 12,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+
+    // スキーマを閲覧する
+    println!("{}", schema.sdl());
+
+    let res = schema.execute("{ hoge { a b } }").await;
+    assert!(res.errors.is_empty());
+    dbg!(res);
+    let res = schema.execute("{ hoge { a } }").await;
+    assert!(res.errors.is_empty());
+    dbg!(res);
+
+    // エラーになる
+    let res = schema.execute("{ hoge { c } }").await;
+    assert!(!res.errors.is_empty());
+
+    let res = schema.execute("{ hoge { d } }").await;
+    assert!(!res.errors.is_empty());
+}

37c6bbe154332dd10cd6bf42e63cf725e78c3bd8
mosson <cucation@gmail.com>
2025-10-10 21:51:23 +0900

    add playground example

---
+++ src/bin/playground.rs
@@ -0,0 +1,72 @@
+use std::sync::Arc;
+
+use async_graphql::{
+    EmptyMutation, EmptySubscription, Object, Response, Schema, Variables,
+    http::{GraphQLPlaygroundConfig, playground_source},
+};
+use axum::{
+    Extension, Json, Router,
+    response::{Html, IntoResponse},
+    routing::get,
+};
+use serde::Deserialize;
+use sqlx::sqlite::SqlitePoolOptions;
+
+#[derive(Deserialize, Debug, Clone)]
+#[allow(dead_code)]
+struct GraphQlRequest {
+    query: String,
+    operation_name: Option<String>,
+    variables: Option<serde_json::Value>,
+}
+
+async fn playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
+async fn graphql_handler(
+    Extension(schema): Extension<Arc<Schema<Query, EmptyMutation, EmptySubscription>>>,
+    Json(payload): Json<GraphQlRequest>,
+) -> Json<Response> {
+    let mut request = async_graphql::Request::new(payload.query);
+    if let Some(operation_name) = payload.operation_name {
+        request = request.operation_name(operation_name);
+    }
+    if let Some(variables) = payload.variables {
+        request = request.variables(Variables::from_json(variables));
+    }
+
+    schema.execute(request).await.into()
+}
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn ping(&self) -> String {
+        "PONG".into()
+    }
+
+    async fn var(&self, id: usize) -> usize {
+        id
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(":memory:")
+        .await
+        .unwrap();
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(pool)
+        .finish();
+
+    let app = Router::new()
+        .route("/", get(playground).post(graphql_handler))
+        .layer(Extension(Arc::new(schema)));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
